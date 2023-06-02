929833c5ec569c061f61cafe3eaad95e0a6edeed
satin <satin3737@gmail.com>
2023-06-02 15:14:04 +0300

    materials les 2

--- src/js/materials.js
+++ src/js/materials.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import "../scss/style.scss";
 import {canvasResponsive} from "./helper.js";
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import * as dat from 'dat.gui';
 
 import baseColorTexture from "../img/door/Door_Wood_001_basecolor.jpg";
 import ambientOcclusionTexture from "../img/door/Door_Wood_001_ambientOcclusion.jpg";
@@ -34,7 +35,20 @@ for (let key in textureRoutes) {
 }
 console.log(texturesObj);
 
+const environmentalMapTexture = new THREE.CubeTextureLoader()
+    .setPath('../environmentMaps/1/')
+    .load([
+        'px.jpg',
+        'nx.jpg',
+        'py.jpg',
+        'ny.jpg',
+        'pz.jpg',
+        'nz.jpg'
+    ]);
+
 const scene = new THREE.Scene();
+
+scene.background = environmentalMapTexture;
 
 const sizes = {
     width: window.innerWidth,
@@ -60,6 +74,15 @@ scene.add(axesHelper);
 
 const orbitControls = new OrbitControls(camera, canvas);
 orbitControls.enableDamping = true;
+
+// add light
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xFFFFFF, 0.5);
+pointLight.position.set(2, 3, 4);
+scene.add(pointLight);
+
 
 // materials
 
@@ -112,17 +135,108 @@ const matcapMaterial = new THREE.MeshMatcapMaterial();
 matcapMaterial.side = THREE.DoubleSide;
 matcapMaterial.matcap = texturesObj.matcapTexture;
 
+
+// MeshDepth material
+// change color according to camera position
+// near - white, far - black
+const meshDepthMaterial = new THREE.MeshDepthMaterial();
+meshDepthMaterial.side = THREE.DoubleSide;
+
+
+// meshLambert material
+// react to lights on scene
+// very good performance
+const meshLambertMaterial = new THREE.MeshLambertMaterial();
+meshLambertMaterial.side = THREE.DoubleSide;
+
+
+// meshPhong material
+// the same as previous but without possible light artifacts and with reflections
+const meshPhongMaterial = new THREE.MeshPhongMaterial();
+meshPhongMaterial.side = THREE.DoubleSide;
+
+// we can control this reflection with shininess property and reflection color with specular property
+meshPhongMaterial.shininess = 100;
+meshPhongMaterial.specular = new THREE.Color(0x1188FF);
+
+
+// meshToon material
+// looks like cartoon, and we can add gradient texture
+const meshToonMaterial = new THREE.MeshToonMaterial({color: 0x1188FF});
+meshToonMaterial.side = THREE.DoubleSide;
+texturesObj.gradientTexture.minFilter = THREE.NearestFilter;
+texturesObj.gradientTexture.magFilter = THREE.NearestFilter;
+texturesObj.gradientTexture.generateMipmaps = false;
+meshToonMaterial.gradientMap = texturesObj.gradientTexture;
+
+
+// meshStandard material
+// it used physically correct rendering
+const meshStandardMaterial = new THREE.MeshStandardMaterial({
+    side: THREE.DoubleSide,
+    color: 0xFFFFFF,
+    // roughness: 0.45,
+    // metalness: 0.8,
+    map: texturesObj.baseColorTexture,
+    aoMap: texturesObj.ambientOcclusionTexture,
+    aoMapIntensity: 1,
+    displacementMap: texturesObj.heightTexture,
+    displacementScale: 0.05,
+    metalnessMap: texturesObj.metalnessTexture,
+    roughnessMap: texturesObj.roughnessTexture,
+    normalMap: texturesObj.normalTexture,
+    transparent: true,
+    alphaMap: texturesObj.opacityTexture
+});
+meshStandardMaterial.normalScale.set(0.5, 0.5);
+
+// aoMap create fake shadows in curves
+// displacementMap create heights on textures
+// displacementScale set that height
+// metalnessMap and roughnessMap don't use with metalness and roughness
+// normalMap added details to texture
+
+
+// meshPhysical material
+// same as standard material but support clear coat effect
+const meshPhysicalMaterial = new THREE.MeshPhysicalMaterial();
+meshPhysicalMaterial.side = THREE.DoubleSide;
+
+
+// debug
+const gui = new dat.GUI();
+const debugObj = {
+    color: 0xFFFFFF
+};
+
+gui.add(meshStandardMaterial, 'metalness').min(0).max(1).step(0.001);
+gui.add(meshStandardMaterial, 'roughness').min(0).max(1).step(0.001);
+gui.add(meshStandardMaterial, 'aoMapIntensity').min(0).max(10).step(0.001);
+gui.add(meshStandardMaterial, 'displacementScale').min(0).max(1).step(0.001);
+gui.addColor(debugObj, 'color').onChange(() => meshStandardMaterial.color.set(debugObj.color));
+
+const setUvCoordinatesForAOMap = (mesh) => {
+    // set uv2 coordinates for ambient occlusion map
+    mesh.geometry.setAttribute(
+        'uv2',
+        new THREE.BufferAttribute(mesh.geometry.attributes.uv.array, 2)
+    );
+}
+
 const createMeshes = (material, positions) => {
     const {spherePos, planePos, torusPos} = positions;
     
-    const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+    const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material);
     sphere.position.set(...spherePos);
+    setUvCoordinatesForAOMap(sphere);
     
-    const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+    const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material);
     plane.position.set(...planePos);
+    setUvCoordinatesForAOMap(plane);
     
-    const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 16, 32), material);
+    const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 64, 128), material);
     torus.position.set(...torusPos);
+    setUvCoordinatesForAOMap(torus);
     
     return [sphere, plane, torus];
 };
@@ -131,13 +245,19 @@ const meshes = [
     ...createMeshes(basicMaterial, {spherePos: [2, 0, 0], planePos: [0, 0, 0], torusPos: [-2, 0, 0]}),
     ...createMeshes(normalMaterial, {spherePos: [2, 0, -2], planePos: [0, 0, -2], torusPos: [-2, 0, -2]}),
     ...createMeshes(matcapMaterial, {spherePos: [2, 0, -4], planePos: [0, 0, -4], torusPos: [-2, 0, -4]}),
+    ...createMeshes(meshDepthMaterial, {spherePos: [2, 0, -6], planePos: [0, 0, -6], torusPos: [-2, 0, -6]}),
+    ...createMeshes(meshLambertMaterial, {spherePos: [2, 0, -8], planePos: [0, 0, -8], torusPos: [-2, 0, -8]}),
+    ...createMeshes(meshPhongMaterial, {spherePos: [2, 0, -10], planePos: [0, 0, -10], torusPos: [-2, 0, -10]}),
+    ...createMeshes(meshToonMaterial, {spherePos: [2, 0, -12], planePos: [0, 0, -12], torusPos: [-2, 0, -12]}),
+    ...createMeshes(meshPhysicalMaterial, {spherePos: [2, 0, -14], planePos: [0, 0, -14], torusPos: [-2, 0, -14]}),
+    ...createMeshes(meshStandardMaterial, {spherePos: [2, 0, 2], planePos: [0, 0, 2], torusPos: [-2, 0, 2]}),
 ];
 scene.add(...meshes);
 
 const animate = () => {
     meshes.forEach(mesh => {
-        mesh.rotation.y += 0.005;
-        mesh.rotation.x += 0.005;
+        mesh.rotation.y += 0.002;
+        mesh.rotation.x += 0.002;
     });
     
     orbitControls.update();
